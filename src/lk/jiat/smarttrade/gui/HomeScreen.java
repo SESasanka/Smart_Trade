e23777d565b688f7dc716a266bbe437b311f7c6a
SESasanka <sasankaakash22@gmail.com>
2025-05-19 22:01:10 +0530

    button add

--- src/lk/jiat/smarttrade/gui/HomeScreen.java
+++ src/lk/jiat/smarttrade/gui/HomeScreen.java
@@ -29,7 +29,7 @@ public class HomeScreen extends javax.swing.JFrame {
     }
     
     private void updateComponents(){
-        jButton1.putClientProperty(FlatClientProperties.STYLE, "arc:50");
+        jButton1.putClientProperty(FlatClientProperties.STYLE, "arc:20");
         SwingUtilities.updateComponentTreeUI(this);
     }
     /**
@@ -44,6 +44,7 @@ public class HomeScreen extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        roundButton1 = new lk.jiat.smarttrade.component.RoundButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -53,6 +54,8 @@ public class HomeScreen extends javax.swing.JFrame {
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/lk/jiat/smarttrade/img/store.png"))); // NOI18N
 
         jButton1.setText("BTN");
+
+        roundButton1.setText("roundButton1");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -68,7 +71,9 @@ public class HomeScreen extends javax.swing.JFrame {
                 .addGap(327, 327, 327))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(279, 279, 279)
-                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(roundButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(292, 292, 292))
         );
         layout.setVerticalGroup(
@@ -80,7 +85,9 @@ public class HomeScreen extends javax.swing.JFrame {
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(52, 52, 52)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(191, 191, 191))
+                .addGap(43, 43, 43)
+                .addComponent(roundButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(125, 125, 125))
         );
 
         pack();
@@ -106,5 +113,6 @@ public class HomeScreen extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private lk.jiat.smarttrade.component.RoundButton roundButton1;
     // End of variables declaration//GEN-END:variables
 }
